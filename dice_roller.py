68b6ef8b3623367f9a1049ac4bd9dcd18801c5c4
Shreya Choure <choureshreya295@gmail.com>
2020-09-10 12:32:14 +0530

    Two dice added

--- dice_roller.py
+++ dice_roller.py
@@ -1,5 +1,15 @@
+import random
 def main():
-  print('You rolled a die')
+  dice_rolls = 2
+  dice_sum = 0
+  for i in range(0, dice_rolls):
+    roll = random.randint(1,6)
+    print(f"You rolled a die {roll}")
+    dice_sum += roll
+  print(f'you scored a total of {dice_sum}')
+    
+  
+  
 
 if __name__== "__main__":
   main()
